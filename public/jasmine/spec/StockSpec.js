89d195dd11a22462dcb70f681996eea564373f42
Arun <mywebcompanion@gmail.com>
2015-11-16 08:23:19 +0800

    Adding some spies

--- public/jasmine/spec/StockSpec.js
+++ public/jasmine/spec/StockSpec.js
@@ -6,6 +6,7 @@ describe("stock",function(){
 
     var stock;
     var originalSharePrice  = 0;
+    var fetchSpy;
 
     beforeEach(function(){
         stock = new Stock({
@@ -14,16 +15,29 @@ describe("stock",function(){
         });
     });
 
-
     describe("when fetched", function() {
         var fetched = false;
         beforeEach(function(){
-           stock.fetch();
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
         });
 
-        it("should update its share price", function(){
-            console.log("After fetch" + stock.sharePrice);
-            expect(stock.sharePrice).toEqual(20.18);
+        it("should update its share price", function(done){
+            consgitole.log("After fetch" + stock.sharePrice);
+            fetchSpy = spyOn(stock,'fetch').and.callThrough();
+            stock.fetch({
+                success : function(){
+                    console.log("Success callback");
+                    var spyCallBack = jasmine.createSpy('myspy');
+                    expect(stock.sharePrice).toEqual(20.18);
+                    stock.callBackMock(spyCallBack);
+                    expect(spyCallBack).toHaveBeenCalled();
+                    done();
+                }
+            });
+            expect(stock.fetch).toHaveBeenCalled();
+        });
+        it('should spy on fetch',function(){
+            expect(fetchSpy).toHaveBeenCalled();
         });
     });
 });
--- public/jasmine/src/Stock.js
+++ public/jasmine/src/Stock.js
@@ -7,12 +7,28 @@ function Stock(params){
     this.sharePrice = params.sharePrice;
 };
 
-Stock.prototype.fetch = function(){
+Stock.prototype.fetch = function(params){
+    console.log("About to fetch stocks from server");
     var that = this;
     var url = 'http://localhost:8000/stocks/' + that.symbol;
+    var success = params.success || function(){};
 
     $.getJSON(url, function(data){
         console.log("Fetched JSON " + data.sharePrice);
         that.sharePrice = data.sharePrice;
+        success(that);
     });
+};
+
+Stock.prototype.callBackMock = function(callback){
+    console.log("Just a dummy print before calling callback");
+    var success = true;
+    if(success){
+        callback();
+    }
+};
+
+Stock.prototype.fakeFetch = function(){
+    console.log("AM being called fakeFetch()");
+    return 10;
 };
